b29730f07eb3b9f044f3cfeb9512033cc5e5dba2
Adam Waldie <aaw4421@rit.edu>
2023-04-12 14:41:54 -0400

    Committed rest of files

--- README.md
+++ README.md
@@ -8,3 +8,14 @@
 ## Description
 
 This project consists of two components. First, a client and server which can send and receive instructions and data. Second, an IDB file containing a fully annotated disassembly of the client. The purpose of this project is to gain a better understanding of reversing Windows programs.
+
+# Compiling
+
+Until I have a better solution like a makefile.
+
+```
+cd client
+g++ client.cpp -o client -lws2_32
+cd ..\server
+g++ server.cpp -o server -lws2_32
+```
--- server/server.h
+++ server/server.h
@@ -1,12 +1,19 @@
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
-#define DEFAULT_BUFLEN 1024
-#define PORT "1337"
+#endif
 
-#include <stdio.h>
-#include <string.h>
+#define _WIN32_WINNT 0x501
+
+#define DEFAULT_PORT "1337"
+#define DEFAULT_BUFLEN 1024
+
 #include <windows.h>
+#include <winsock2.h>
 #include <ws2tcpip.h>
-#include <WinSock2.h>
+#include <iphlpapi.h>
+#include <stdio.h>
 
 #pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
 
